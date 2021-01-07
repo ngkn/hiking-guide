ab0c50240c544d2ff8e700d3b57881c7e4ee5962
ngkn <ngouayeke.nathanael@gmail.com>
2021-01-07 23:54:32 +0100

    feat(component): add footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,49 @@
 import React from "react"
 
+import Logo from "./svg/Logo"
+import infos from "config/infos"
+
 const Footer = () => {
   return (
     <footer>
-      <div className={"footer"}>
-        <div className={"footer-logo"}>logo</div>
-        <div className={"footer-list"}>
-          <div className={"footer-blog"}>blog</div>
-          <div className={"footer-more"}>more</div>
+      <div className="footer">
+        <div className="footer-logo-text">
+          <i className="logo">
+            <Logo />
+          </i>
+          <p className="footer-text">
+            Get out there & discover your next slope, mountain & destination!
+          </p>
+          <div className="copyright">
+            Copyright 2019 MNTN, Inc. Terms & Privacy
+          </div>
         </div>
-      </div>
-      <div className={"copyright"}>
-        Copyright 2019 MNTN, Inc. Terms & Privacy
+        <div className="footer-list">
+          <div className="footer-section">
+            <h3 className="footer-title">More on the blog</h3>
+            <nav className="footer-nav">
+              <ul className="footer-links-container">
+                {infos.footerBlogLinks.map((link) => (
+                  <li key={link.title} className="footer-link">
+                    <a href={link.link}>{link.title}</a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+          <div className="footer-section">
+            <h3 className="footer-title">More on MNTN</h3>
+            <nav className="footer-nav">
+              <ul className="footer-links-container">
+                {infos.footerMoreInfoLinks.map((link) => (
+                  <li key={link.title} className="footer-link">
+                    <a href={link.link}>{link.title}</a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+        </div>
       </div>
     </footer>
   )

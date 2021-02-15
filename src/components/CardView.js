53126337792371535afd8f7aaae483636d83f0c6
ngkn <ngouayeke.nathanael@gmail.com>
2021-02-15 22:34:07 +0100

    feat(component): add cardView component
    - Add the cardView component
    - Handler it's UI
    - Handler responsive images with picture tag (Art direction/ Perf)
    - Create different format of images to be load on the best devices.

--- src/components/CardView.js
+++ src/components/CardView.js
@@ -2,38 +2,91 @@ import React from "react"
 import PropTypes from "prop-types"
 
 const CardView = ({ card }) => {
-  return (
-    <div className="card">
-      {/* <div className="card__number-container">
-        <span className="card__number">{card.number}</span>
-      </div> */}
-      <div className="card__description">
-        <span className="card__number">{card.number}</span>
-        <div className="card__tagline-container">
-          <hr className="card__line" />
-          <h3 className="card__tagline">{card.tagline}</h3>
-        </div>
-        <h2 className="card__title">{card.title}</h2>
-        <div className="card__text">{card.text}</div>
-      </div>
+  const isOdd = !!(Number(card.number) % 2) // Determine odd of a card to apply it specifics css properties
+  const cardEvenClassName = isOdd ? "" : "card--even"
+  const cardContainerEvenClassName = isOdd ? "" : "card__container--even"
+  const cardDescriptionEvenClassName = isOdd
+    ? "card__description--odd"
+    : "card__description--even"
 
+  const cardTaglineEvenClassName = isOdd
+    ? "card__tagline--odd"
+    : "card__tagline--even"
+  const cardLineEvenClassName = isOdd ? "card__line--odd" : "card__line--even"
+  const cardNumberEvenClassName = isOdd
+    ? "card__number--odd"
+    : "card__number--even"
+
+  return (
+    <div className={`card ${cardEvenClassName}`}>
+      <div className={`card__container ${cardContainerEvenClassName}`}>
+        <div className={`card__description ${cardDescriptionEvenClassName}`}>
+          <span className={`card__number ${cardNumberEvenClassName}`}>
+            {card.number}
+          </span>
+          <hr className={`card__line ${cardLineEvenClassName}`} />
+          <h3 className={`card__tagline ${cardTaglineEvenClassName}`}>
+            {card.tagline}
+          </h3>
+          <h2 className="card__title">{card.title}</h2>
+          <div className="card__text">{card.text}</div>
+        </div>
+      </div>
       <div className="card__image-container">
-        <img className="card__image" src={card.image} />
+        <picture>
+          <source
+            media={card.images.small.source.media}
+            srcSet={card.images.small.source.srcset}
+          />
+          <source
+            media={card.images.medium.source.media}
+            srcSet={card.images.medium.source.srcset}
+            size={card.images.medium.source.sizes}
+          />
+          <source
+            media={card.images.large.source.media}
+            srcSet={card.images.large.source.srcset}
+            size={card.images.large.source.sizes}
+          />
+          <img className="card__image" src={card.images.src} alt={card.alt} />
+        </picture>
       </div>
     </div>
   )
 }
 
 CardView.propTypes = {
-  //   card: PropTypes.shape({
-  //     number: PropTypes.string.isRequired,
-  //     tagline: PropTypes.string.isRequired,
-  //     title: PropTypes.string.isRequired,
-  //     text: PropTypes.string.isRequired,
-  //     image: PropTypes.string.isRequired,
-  //     link: PropTypes.string.isRequired,
-  //   }),
-  card: PropTypes.objectOf(PropTypes.string),
+  card: PropTypes.shape({
+    number: PropTypes.string.isRequired,
+    tagline: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    images: PropTypes.shape({
+      small: PropTypes.shape({
+        source: PropTypes.shape({
+          media: PropTypes.string,
+          srcset: PropTypes.string,
+        }),
+      }),
+      medium: PropTypes.shape({
+        source: PropTypes.shape({
+          media: PropTypes.string,
+          srcset: PropTypes.string,
+          sizes: PropTypes.string,
+        }),
+      }),
+      large: PropTypes.shape({
+        source: PropTypes.shape({
+          media: PropTypes.string,
+          srcset: PropTypes.string,
+          sizes: PropTypes.string,
+        }),
+      }),
+      src: PropTypes.string.isRequired,
+    }),
+    link: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+  }),
 }
 
 export default CardView
--- src/config/infos.js
+++ src/config/infos.js
@@ -1,3 +1,10 @@
+import hikerWalkingSmall540 from "images/optimizeImages/climb-walking-small.jpg"
+import hikerWalkingSmall920 from "images/optimizeImages/climb-walking-small@3x.jpg"
+import hikerWalkingMedium856 from "images/optimizeImages/climb-walking-medium.jpg"
+import hikerWalkingMedium1284 from "images/optimizeImages/climb-walking-medium@3x.jpg"
+import hikerWalkingLarge522 from "images/optimizeImages/climb-walking-large.jpg"
+import hikerWalkingLarge1044 from "images/optimizeImages/climb-walking-large@3x.jpg"
+
 export default {
   sidebarLinks: [
     {
@@ -46,6 +53,106 @@ export default {
     },
     {
       title: "Press",
+      link: "#",
+    },
+  ],
+  cardInfoHiking: [
+    {
+      number: "01",
+      tagline: "get started",
+      title: "What level of hiker are you?",
+      text:
+        "Determining what level of hiker you are can be an important tool when planning future hikes. This hiking level guide will help you plan hikes according to different hike ratings set by various websites like All Trails and Modern Hiker. What type of hiker are you – novice, moderate, advanced moderate, expert, or expert backpacker?",
+      images: {
+        small: {
+          source: {
+            media: "(max-width: 767px)",
+            // Multiple images are used in srcset to match with Retina screen. For that we need to double width/pixels (x2, x3...)
+            srcset: `${hikerWalkingSmall540} 540w, ${hikerWalkingSmall920} 920w`,
+          },
+        },
+        medium: {
+          source: {
+            media: "(min-width: 768px) and (max-width: 1023px)",
+            srcset: `${hikerWalkingMedium856} 900w,  ${hikerWalkingMedium1284} 1284w`,
+            // Because image take 50% of the main block (text at left, image at right). In mobile it take all the width of the device
+            sizes: "(min-width: 768px) 50vw , 100vw",
+          },
+        },
+        large: {
+          source: {
+            media: "(min-width: 1024px)",
+            srcset: `${hikerWalkingLarge522} 522w,  ${hikerWalkingLarge1044} 1044w`,
+            sizes: "(min-width: 1024px) 50vw , 100vw",
+          },
+        },
+        src: hikerWalkingSmall540,
+      },
+      alt: "Hiker walking on a mountain",
+      link: "#",
+    },
+    {
+      number: "02",
+      tagline: "hiking essentials",
+      title: "Picking the right Hiking Gear!",
+      text:
+        "The nice thing about beginning hiking is that you don’t really need any special gear, you can probably get away with things you already have. Let’s start with clothing. A typical mistake hiking beginners make is wearing jeans and regular clothes, which will get heavy and chafe wif they get sweaty or wet.",
+      images: {
+        small: {
+          source: {
+            media: "(max-width: 767px)",
+            srcset: `${hikerWalkingSmall540} 540w, ${hikerWalkingSmall920} 920w`,
+          },
+        },
+        medium: {
+          source: {
+            media: "(min-width: 768px) and (max-width: 1023px)",
+            srcset: `${hikerWalkingMedium856} 900w,  ${hikerWalkingMedium1284} 1284w`,
+            sizes: "(min-width: 768px) 50vw , 100vw",
+          },
+        },
+        large: {
+          source: {
+            media: "(min-width: 1024px)",
+            srcset: `${hikerWalkingLarge522} 522w,  ${hikerWalkingLarge1044} 1044w`,
+            sizes: "(min-width: 1024px) 50vw , 100vw",
+          },
+        },
+        src: hikerWalkingSmall540,
+      },
+      alt: "Hiker standing at the top of a mountain",
+      link: "#",
+    },
+    {
+      number: "03",
+      tagline: "where you go is the key",
+      title: "Understand Your Map & Timing",
+      text:
+        "To start, print out the hiking guide and map. If it’s raining, throw them in a Zip-Lock bag. Read over the guide, study the map, and have a good idea of what to expect. I like to know what my next landmark is as I hike. For example, I’ll read the guide and know that say, in a mile, I make a right turn at the junction..",
+      images: {
+        small: {
+          source: {
+            media: "(max-width: 767px)",
+            srcset: `${hikerWalkingSmall540} 540w, ${hikerWalkingSmall920} 920w`,
+          },
+        },
+        medium: {
+          source: {
+            media: "(min-width: 768px) and (max-width: 1023px)",
+            srcset: `${hikerWalkingMedium856} 900w,  ${hikerWalkingMedium1284} 1284w`,
+            sizes: "(min-width: 768px) 50vw , 100vw",
+          },
+        },
+        large: {
+          source: {
+            media: "(min-width: 1024px)",
+            srcset: `${hikerWalkingLarge522} 522w,  ${hikerWalkingLarge1044} 1044w`,
+            sizes: "(min-width: 1024px) 50vw , 100vw",
+          },
+        },
+        src: hikerWalkingSmall540,
+      },
+      alt: "Hiker showing a compass above a mountain",
       link: "#",
     },
   ],
